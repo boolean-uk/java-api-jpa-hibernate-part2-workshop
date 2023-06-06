56ef8bc3374e4060faace230959dc3bd0197fc7d
Kitti <kitti.horvath6891@gmail.com>
2023-06-06 12:46:25 +0200

    morning exercise completed

---
+++ src/main/java/com/booleanuk/api/model/Employee.java
@@ -0,0 +1,67 @@
+package com.booleanuk.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    @JsonIgnoreProperties("employees")
+    private Department department;
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(String firstName, String lastName) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
