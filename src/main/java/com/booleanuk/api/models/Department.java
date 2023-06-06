4318dc5af4799dcf9041cceb8d731ec4374c46f7
Emil Solgaard <emil.solgaard@dk.experis.com>
2023-06-06 12:09:25 +0200

    Departments + Employees DTO solution

---
+++ src/main/java/com/booleanuk/api/models/Department.java
@@ -0,0 +1,60 @@
+package com.booleanuk.api.models;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "location")
+    private String location;
+    @OneToMany(mappedBy = "department")
+    //@JsonManagedReference
+    private List<Employee> employees = new ArrayList<Employee>();
+
+    public Department() {
+        super();
+    }
+    public Department(String name, String location) {
+        super();
+        this.name = name;
+        this.location = location;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
